498a705182d22ee222aa3c84ab21f686be57248b
Carter <speers.carter@gmail.com>
2024-06-27 15:24:21 -0400

    First Commit v.1.0.3
    First Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+### Gitty API
+
+gitty api is a set of functions that allows you to interface with your github repositories at a high level.
+
+### API token
+
+gitty api uses your api token to make requests. Please set an environment variable called `GH_API_TOKEN`
+set to your token to successfully call these functions.
---
+++ index.ts
@@ -0,0 +1,134 @@
+export interface Project {
+  name: string,
+  owner: string,
+  private: boolean,
+  url: string,
+  description: string,
+  stars: number,
+  watchers: number,
+  forks: number,
+  license: string,
+}
+
+
+
+export default async function getAllRepos(username: string): Promise<Project[]> {
+
+  let url = `https://api.github.com/users/${username}/repos`
+  const projects: Project[] = []
+  let page = 1;
+  let hasNextPage = true;
+
+  while (hasNextPage) {
+    const response = await fetch(
+      `${url}?page=${page}&per_page=100`, {
+      method: 'GET',
+
+      headers: {
+
+        'Authorization': `token ${process.env.GH_API_TOKEN}`
+      }
+    });
+
+    const data = await response.json();
+
+
+    let project: Project;
+    data.forEach((repo: any) => {
+
+      project = {
+        name: repo.name,
+        owner: repo.owner.login,
+        private: repo.private,
+        url: repo.html_url,
+        description: repo.description,
+        stars: repo.stargazers_count,
+        watchers: repo.watchers_count,
+        forks: repo.forks,
+        license: repo.license
+      }
+
+      projects.push(project);
+
+    });
+
+    page++;
+    hasNextPage = data.length === 100
+  }
+
+
+
+  return projects;
+}
+
+export async function getReposWithStars(username: string, stars: number): Promise<Project[]> {
+  let allProjects = await getAllRepos(username);
+
+  return allProjects.filter((value, _index, _arr) => {
+
+    if (value.stars >= stars)
+      return value
+  })
+}
+
+export async function getReposWithName(username: string, name: string): Promise<Project[]> {
+
+  let allProjects = await getAllRepos(username);
+
+  return allProjects.filter((value, _index, _arr) => {
+
+    if (value.name === name)
+      return value
+  })
+}
+export async function getReposWithForks(username: string, forks: number): Promise<Project[]> {
+
+  let allProjects = await getAllRepos(username);
+
+  return allProjects.filter((value, _index, _arr) => {
+
+    if (value.forks >= forks)
+      return value
+  })
+}
+export async function getReposWithDescription(username: string, description: string): Promise<Project[]> {
+
+  let allProjects = await getAllRepos(username);
+
+  return allProjects.filter((value, _index, _arr) => {
+
+    if (value.description === description)
+      return value
+  })
+}
+export async function getReposWithWatchers(username: string, watchers: number): Promise<Project[]> {
+
+  let allProjects = await getAllRepos(username);
+
+  return allProjects.filter((value, _index, _arr) => {
+
+    if (value.watchers >= watchers)
+      return value
+  })
+}
+
+export async function getPrivateRepos(username: string): Promise<Project[]> {
+
+  let allProjects = await getAllRepos(username);
+
+  return allProjects.filter((value, _index, _arr) => {
+
+    if (value.private)
+      return value
+  })
+}
+export async function getReposWithLicense(username: string, license: string): Promise<Project[]> {
+
+  let allProjects = await getAllRepos(username);
+
+  return allProjects.filter((value, _index, _arr) => {
+
+    if (value.license === license)
+      return value
+  })
+}
